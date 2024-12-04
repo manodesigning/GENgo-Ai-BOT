200e08c62b521787e498115fd41739be9a8c48cd
manodesigning <manoharan.m.designing@gmail.com>
2024-12-04 15:21:26 +0800

    glassy style update

--- src/Components/ChatBox/ChatBox.jsx
+++ src/Components/ChatBox/ChatBox.jsx
@@ -109,7 +109,7 @@ const ChatBox = () => {
         `}
       </style>
 
-      <div className="flex-1 w-full overflow-y-auto mb-4 space-y-4 p-4  rounded-lg">
+      <div className=" bg-white/30 backdrop-blur-md shadow-lg flex-1 w-full overflow-y-auto mb-4 space-y-4 p-4  rounded-lg">
         {/* Top Button */}
         <div className="flex justify-center items-center">
           <ButtonGrid sendMessage={sendMessage} />
